7392920331b17883e800af6091804dd8638527b1
Bruno Felix <brunofelixpinto@gmail.com>
2025-06-07 20:40:47 -0300

    feat: modifying oklch to hex at index.css

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button className="bg-black text-white">ola mundo</Button>
+      <Button className="bg-black text-white">ola mundo, testando o tailwind e o shadcn</Button>
     </div>
   )
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+//essa util é só pra o shadcn mesmo
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
