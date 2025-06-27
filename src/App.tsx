e9fd11c69d103810a3c3e7bdf5c12d3b977e5571
Bruno Felix <brunofelixpinto@gmail.com>
2025-06-27 14:55:18 -0300

    feat: adding router and testing shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@/components/ui/button"
+import { Card, CardHeader } from "./components/ui/card"
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
+      <Card>
+        <CardHeader>
+          Teste
+        </CardHeader>
       <Button className="bg-black text-white">ola mundo, testando o tailwind e o shadcn</Button>
+
+      </Card>
     </div>
   )
 }
